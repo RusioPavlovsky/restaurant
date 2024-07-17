cb85e2b58c2b84d2f1328fa9f1eec962f88a3d57
Ruslan Pavlov <rpavlov@ubic.tech>
2024-07-17 18:22:45 +0300

    initial commit

---
+++ src/test/java/rusio/pavlovsky/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package rusio.pavlovsky.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
